508b50562c0771b28c33381b693329460f4eccf2
Noé Mérida <20213tn121@utez.edu.mx>
2024-02-07 10:11:25 -0600

    [ADD] The Profile Page has been added

--- frontend/src/modules/user/views/Perfil.vue
+++ frontend/src/modules/user/views/Perfil.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
     <h1>Perfil</h1>
+    <div class="container">
+      <div class="card">
+        <p>Nombre</p>
+        <p class="p-input">Noe</p>
+        <p>Apellido Paterno</p>
+        <p class="p-input">Merida</p>
+        <p>Apellido Materno</p>
+        <p class="p-input">Puente</p>
+        <p>Correo Electronico</p>
+        <p class="p-input">20213tn121@utez.edu.mx</p>
+        <b-button block href="/login" class="btn" variant="primary">Cerrar sesión</b-button>
+      </div>
+
+    </div>
+
   </div>
 </template>
 
@@ -10,6 +25,42 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style>
+.container {
+  max-width: 1100px;
+  margin: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(80vh - 60px);
+}
 
+.card {
+  max-width: 1100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.p-input {
+  border: 0.5px solid black;
+  border-radius: 4px;
+  border-color: black;
+  width: 500px;
+  font-weight: normal;
+  text-align: left;
+  font-size: large;
+}
+
+p {
+  font-weight: bolder;
+  font-size: large;
+  
+}
+
+.btn {
+  width: 500px;
+  margin-top: 30px;
+}
 </style>
