ea8941fddbea1c3b36d55b9f62f85533399979cf
MeridaNoe <20213tn121@utez.edu.mx>
2024-03-15 15:31:20 -0600

    [ADD] Cambios en estilos

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -5,7 +5,6 @@
         <div class="d-flex justify-content-center align-items-center">
           <img src="../assets/img/logo.png" alt="">
         </div>
-
         <h1>Hospital</h1>
       </template>
       <template #content>
@@ -13,24 +12,23 @@
           <b-col class="mt-4 mb-2" lg="12">
             <div class="field w-100">
               <span class="p-float-label p-input-icon-right">
-                <InputText id="username" type="text" v-model="value" />
+                <i class="pi pi-user"></i>
+                <InputText id="username" type="text" v-model="username" />
                 <label for="username">Username</label>
               </span>
             </div>
           </b-col>
         </b-row>
-
         <b-col class="mt-4 mb-2" lg="12">
           <div class="field">
             <span class="p-float-label p-input-icon-right">
-              <InputText type="password" v-model="password"></InputText>
+              <i class="pi" :class="inputType == 'text' ? 'pi-eye-slash' : 'pi-eye'" @click="setTypeInput(inputType)"></i>
+              <InputText :type="inputType" v-model="password"></InputText>
               <label for="password">Password</label>
             </span>
           </div>
         </b-col>
-        <div>
           <Button class="p-button-rounded" label="Inicar Sesion" />
-        </div>
       </template>
     </Card>
   </div>
@@ -55,71 +53,85 @@ export default {
     return {
       isLoading: true,
       password: null,
+      inputType: 'password',
+      username: null
     }
   },
+  methods: {
+    setTypeInput(t) {
+      if(t === 'text') {
+        this.inputType = 'password';
+        return;
+      }
+      this.inputType = 'text';
+    },
+  }
 
 }
 </script>
-<style scoped>
-#login {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  background-color: #2a715a;
-}
-
-
-img {
-  max-width: 100%;
-  max-height: 350px;
-  width: auto;
-  height: auto;
-}
-
-.cards {
-  width: 800px;
-  height: 85%;
-  border: 1px solid #ccc;
-  border-radius: 10px 10px !important;
-  overflow: auto;
-}
-
-h1 {
-  font-weight: bolder !important;
-  font-family: Arial, Helvetica, sans-serif !important;
-}
-
-.p-button {
-  width: 50%;
-  margin-top: 50px !important;
-  background-color: #2383f0 !important;
-  border: 0px solid !important;
-}
-
-.p-inputtext {
-  width: 380px;
-}
-
-Button {
-  width: 380px;
-}
-
-@media(max-width: 768px){
-  .p-button{
-    width: 100%;
+  <style scoped>
+  #login {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #2a715a;
   }
-}
 
 
-@media (max-width: 768px) {
+  img {
+    max-width: 100%;
+    max-height: 350px;
+    width: auto;
+    height: auto;
+  }
+
+  .cards {
+    width: 800px;
+    height: 750px;
+    border: 1px solid #ccc;
+    border-radius: 10px 10px !important;
+    overflow: hidden;
+  }
+  .pi{
+    cursor: pointer;
+  }
+
+  h1 {
+    font-weight: bolder !important;
+    font-family: Arial, Helvetica, sans-serif !important;
+  }
+
+  .p-button {
+    width: 50%;
+    margin-top: 20px !important;
+    background-color: #2383f0 !important;
+    border: 0px solid !important;
+  }
+
   .p-inputtext {
-    width: 100%;
+    width: 380px;
   }
 
-}
+  Button {
+    width: 380px;
+  }
 
-.inputs {
-  margin-top: 25px;
-}
-</style>
+  @media(max-width: 768px){
+    .p-button{
+      width: 100%;
+    }
+  }
+
+
+  @media (max-width: 768px) {
+    .p-inputtext {
+      width: 100%;
+    }
+
+  }
+
+  .inputs {
+    margin-top: 25px;
+  }
+  </style>
